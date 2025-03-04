97fa7f14d51e0875afb3676510e6307973d828a0
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2025-03-04 20:12:28 +0700

    Update data.js

--- data.js
+++ data.js
@@ -25,10 +25,22 @@ export default {
       const entityRes = await fetch(`https://www.wikidata.org/wiki/Special:EntityData/${wikidataId}.json`);
       const entityData = await entityRes.json();
       const entity = entityData.entities[wikidataId]?.claims;
+      let entityDesc = entityData.entities[wikidataId]?.descriptions?.en?.value || "No description";
 
-      const isImportant = ["Q5", "Q6256", "Q4830453"].some(type => entity["P31"]?.some(e => e.mainsnak?.datavalue?.value?.id === type));
       const isCountry = entity["P31"]?.some(e => e.mainsnak?.datavalue?.value?.id === "Q6256");
+      const isHuman = entity["P31"]?.some(e => e.mainsnak?.datavalue?.value?.id === "Q5");
       const isCompany = entity["P31"]?.some(e => e.mainsnak?.datavalue?.value?.id === "Q4830453");
+
+      async function getRelatedImages(title) {
+        const imagesRes = await fetch(`https://commons.wikimedia.org/w/api.php?action=query&format=json&generator=images&titles=${encodeURIComponent(title)}&gimlimit=5&prop=imageinfo&iiprop=url|thumburl`);
+        const imagesJson = await imagesRes.json();
+
+        return Object.values(imagesJson?.query?.pages || {})
+          .map(img => img.imageinfo?.[0]?.thumburl || img.imageinfo?.[0]?.url)
+          .filter(Boolean);
+      }
+
+      const relatedImages = await getRelatedImages(wikiData.title);
 
       async function getValue(prop, label, isDate = false, multiple = false) {
         const values = entity[prop]?.map(e => e.mainsnak?.datavalue?.value).filter(Boolean) || [];
@@ -47,69 +59,47 @@ export default {
           return data;
         }));
 
-        return resultValues.length > 0 ? { label, value: multiple ? resultValues.join(", ") : resultValues[0] } : null;
+        return { label, value: multiple ? resultValues.join(", ") : resultValues[0] };
       }
 
-      let infobox = [];
+      let infobox = (await Promise.all([
+        isCountry ? getValue("P36", "Ibu kota") : null,
+        isCountry ? getValue("P35", "Presiden") : null,
+        isCountry ? getValue("P6", "Perdana Menteri") : null,
+        isCountry ? getValue("P1082", "Jumlah penduduk") : null,
+        isCountry ? getValue("P30", "Benua") : null,
+        isCompany ? getValue("P112", "Pendiri", false, true) : null,
+        isCompany ? getValue("P169", "CEO") : null,
+        isCompany ? getValue("P159", "Kantor pusat") : null,
+        isCompany ? getValue("P17", "Negara asal") : null,
+        isHuman ? getValue("P569", "Tanggal lahir", true) : null,
+        isHuman ? getValue("P69", "Pendidikan", false, true) : null,
+      ])).filter(Boolean);
 
       if (isCountry) {
-        infobox = (await Promise.all([
-          getValue("P571", "Didirikan", true),
-          getValue("P36", "Ibu kota"),
-          getValue("P6", "Presiden"),
-          getValue("P35", "Perdana Menteri"),
-          getValue("P1082", "Jumlah penduduk"),
-          getValue("P473", "Kode telepon"),
-          getValue("P30", "Benua"),
-        ])).filter(Boolean);
-      } else if (isCompany) {
-        infobox = (await Promise.all([
-          getValue("P571", "Didirikan", true),
-          getValue("P112", "Pendiri", false, true),
-          getValue("P169", "CEO"),
-          getValue("P159", "Kantor pusat"),
-          getValue("P17", "Negara asal"),
-          getValue("P2541", "Wilayah operasi", false, true),
-        ])).filter(Boolean);
+        infobox = infobox.filter(item => item.label !== "Didirikan");
       }
 
-      if (infobox.length < 5) {
-        const extraProps = await Promise.all([
-          getValue("P856", "Situs web"),
-          getValue("P625", "Koordinat"),
-          getValue("P102", "Partai politik"),
-          getValue("P69", "Pendidikan"),
-        ]);
-        infobox.push(...extraProps.filter(Boolean));
-      }
-
-      infobox = infobox.filter(item => !(item.label === "Koordinat"));
-
-      async function getLogo() {
-        if (entity["P154"]) {
-          const logoId = entity["P154"][0]?.mainsnak?.datavalue?.value;
-          return `https://commons.wikimedia.org/wiki/Special:FilePath/${encodeURIComponent(logoId)}`;
+      if (isCompany) {
+        let logo = entity["P154"]?.[0]?.mainsnak?.datavalue?.value || null;
+        if (logo) {
+          logo = `https://commons.wikimedia.org/wiki/Special:FilePath/${encodeURIComponent(logo)}?width=200`;
         }
-        return null;
+        wikiData.originalimage = { source: logo };
       }
 
-      async function getRelatedImages(title) {
-        const imagesRes = await fetch(`https://commons.wikimedia.org/w/api.php?action=query&format=json&generator=images&titles=${encodeURIComponent(title)}&gimlimit=5&prop=imageinfo&iiprop=url|thumburl`);
-        const imagesJson = await imagesRes.json();
-
-        return Object.values(imagesJson?.query?.pages || {})
-          .map(img => img.imageinfo?.[0]?.thumburl || img.imageinfo?.[0]?.url) // Pakai thumbnail kecil dulu, kalau gak ada pakai yang besar
-          .filter(Boolean);
+      if (isCountry) {
+        const populationItem = infobox.find(item => item.label === "Jumlah penduduk");
+        if (populationItem) {
+          populationItem.value = populationItem.value.toString();
+        }
       }
-
-      const logo = await getLogo();
-      const relatedImages = isImportant ? await getRelatedImages(wikiData.title) : [];
 
       const result = {
         title: wikiData.title,
-        type: entityData.entities[wikidataId]?.descriptions?.en?.value || "No description",
+        type: entityDesc,
         description: wikiData.extract,
-        logo,
+        image: wikiData.originalimage?.source || null,
         related_images: relatedImages,
         infobox,
         source: "Wikipedia",
